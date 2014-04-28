d10fdfe6834a37afa37393850495d73a2cedd8d1
cescoc <cesarecocito@gmail.com>
2014-04-28 22:29:33 +0200

    Corrections in Gruntfile.js

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
                     'js/src/script.js'
                 ],
                 dest: '{%= buildPath %}/{%= jsFolder %}/script.js'
-            }
+            },
             css_dev: {
                 src: [
                     // Stylesheets other then main.css is treated as a shame-sheet
